3ea66a56e1034a85118c4a4ae8c186ad372f8a4a
Christine Seddon <noddes@gmail.com>
2021-11-24 12:21:24 -0600

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const divide = (num1 = 2000, num2 = 100) => {
+return num1 / num2;
+}
+divide
+
+const square = (x = 2) => {
+return x * x;
+}
+square
+
+const add = (a = 3, b = 4) => {
+    return a + b;
+}
+add
